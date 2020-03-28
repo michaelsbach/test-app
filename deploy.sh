29d94163a28656592e69a88f2a5edc540a295f2d
michaelsbach <m.sbach24@gmail.com>
2020-03-28 03:16:30 +0000

    adding deployment scripts to deploy.sh and .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,16 @@ docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 # Log in to Docker Hub and push
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${IMAGE}:${GIT_VERSION}
+
+
+#from here, need to use digital ocean's "doctl" CLI commands
+
+#find current running container and stop it 
+		#(You can do this by setting a --name on the container, and stopping and removing that container before setting the new one)
+
+#start up new container in bg
+#run server's local port on 80 (ie, so can access example.com instead of example.com:5000)
+  # docker run --name=curr_container --restart-unless-stopped -d -p 80:5000
+#prune all old containers and images
+  # docker system prune -a -f
+
